0da29573d26e236f0f2901f45000d6c2e17c4319
Joh Yoshida <johyoshida@gmail.com>
2020-12-16 19:01:32 -0800

    Added default Material Design colors for dark and light themes

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,70 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const tintLight = "#2f95dc";
+const tintDark = "#fff";
+const primaryLight = "#6200ee";
+const primaryDark = "#bb86fc";
+const primaryVariantLight = "#3700b3";
+const primaryVariantDark = "#3700b3";
+const secondaryLight = "#03dac6";
+const secondaryDark = "#03dac6";
+const secondaryVariantLight = "#018786";
+const secondaryVariantDark = "#03dac6";
+const backgroundLight = "#fff";
+const backgroundDark = "#121212";
+const surfaceLight = "#fff";
+const surfaceDark = "#121212";
+const errorLight = "#b00020";
+const errorDark = "#cf6679";
+const onPrimaryLight = "#fff";
+const onPrimaryDark = "#000";
+const onSecondaryLight = "#000";
+const onSecondaryDark = "#000";
+const onBackgroundLight = "#000";
+const onBackgroundDark = "#fff";
+const onSurfaceLight = "#000";
+const onSurfaceDark = "#fff";
+const onErrorLight = "#fff";
+const onErrorDark = "#000";
+const cancelLight = "#ef9a9a";
+const cancelDark = "#ef9a9a";
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    text: "#000",
+    primary: primaryLight,
+    primaryVariant: primaryVariantLight,
+    secondary: secondaryLight,
+    secondaryVariant: secondaryVariantLight,
+    background: backgroundLight,
+    surface: surfaceLight,
+    error: errorLight,
+    onPrimary: onPrimaryLight,
+    onSecondary: onSecondaryLight,
+    onBackground: onBackgroundLight,
+    onSurface: onSurfaceLight,
+    onError: onErrorLight,
+    tint: tintLight,
+    cancel: cancelLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintLight
   },
   dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+    text: "#fff",
+    primary: primaryDark,
+    primaryVariant: primaryVariantDark,
+    secondary: secondaryDark,
+    secondaryVariant: secondaryVariantDark,
+    background: backgroundDark,
+    surface: surfaceDark,
+    error: errorDark,
+    onPrimary: onPrimaryDark,
+    onSecondary: onSecondaryDark,
+    onBackground: onBackgroundDark,
+    onSurface: onSurfaceDark,
+    onError: onErrorDark,
+    tint: tintDark,
+    error: errorDark,
+    cancel: cancelDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintDark
+  }
 };
