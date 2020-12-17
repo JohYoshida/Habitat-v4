a6f314165db146506c4451911157de36b69478f0
Joh Yoshida <johyoshida@gmail.com>
2020-12-16 16:28:10 -0800

    Require and load fonts for NativeBase

--- App.tsx
+++ App.tsx
@@ -1,17 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppLoading from 'expo-app-loading';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+} from '@expo-google-fonts/roboto';
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
+  let [fontsLoaded] = useFonts({
+    'Roboto': Roboto_400Regular,
+    'Roboto_medium': Roboto_500Medium,
+  });
+
+  if (!fontsLoaded || !isLoadingComplete) {
+    return <AppLoading />;
   } else {
     return (
       <SafeAreaProvider>
