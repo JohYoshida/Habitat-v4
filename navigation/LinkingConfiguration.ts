64807894b42a0ad8ff8b25c761ad5b89fedf13d6
Joh Yoshida <johyoshida@gmail.com>
2020-12-16 16:35:26 -0800

    Changed TabOne and TabTwo to Exercise and Settings respectively

--- navigation/LinkingConfiguration.ts
+++ navigation/LinkingConfiguration.ts
@@ -1,24 +1,24 @@
-import * as Linking from 'expo-linking';
+import * as Linking from "expo-linking";
 
 export default {
-  prefixes: [Linking.makeUrl('/')],
+  prefixes: [Linking.makeUrl("/")],
   config: {
     screens: {
       Root: {
         screens: {
-          TabOne: {
+          Exercise: {
             screens: {
-              TabOneScreen: 'one',
-            },
+              ExerciseScreen: "exercise"
+            }
           },
-          TabTwo: {
+          Settings: {
             screens: {
-              TabTwoScreen: 'two',
-            },
-          },
-        },
+              SettingsScreen: "settings"
+            }
+          }
+        }
       },
-      NotFound: '*',
-    },
-  },
+      NotFound: "*"
+    }
+  }
 };
