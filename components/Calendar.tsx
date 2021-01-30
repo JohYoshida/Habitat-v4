6339eaef5e958f58fd05b057f6d5a738eee7b7f1
Joh Yoshida <johyoshida@gmail.com>
2021-01-29 23:14:59 -0800

    Skeleton

---
+++ components/Calendar.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import {StyleSheet} from "react-native";
+import {Text, View} from '../components/Themed';
+import Colors from "../constants/Colors";
+import useColorScheme from '../hooks/useColorScheme';
+
+export default function Calendar(props) {
+  const colorScheme = useColorScheme();
+
+  return (<View></View>);
+}
+
+const styles = StyleSheet.create({});
